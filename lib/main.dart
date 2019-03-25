a85b816ad5cc5b05026eacea20cb098b22892898
samar <smrlqzlbsh@gmail.com>
2019-03-25 14:26:54 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'features/main_page.dart';
+
+void main() => runApp(MyApp());
---
+++ lib/utilz.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class Utilz {
+  
+  static const APP_BG = Color(0xFFeee9d6);
+
+
+}
